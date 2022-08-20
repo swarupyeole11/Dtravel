6effc70c2ffccb9b9bcaa176aac1beff00f00f01
Swar <swar@pop-os.localdomain>
2022-08-21 00:59:23 +0530

    dtrl

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const style ={
+     wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60`
+}
+
+
+const Navbar = () => {
+  return (
+       <div className={style.wrapper}></div>
+  )
+}
+
+export default Navbar
